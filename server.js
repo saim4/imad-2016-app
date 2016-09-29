92b833e41cb565b7d3229b11ffb723a4efa62e1e
saim4 <saimyusuf4@gmail.com>
2016-09-29 12:31:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,9 +21,7 @@ app.get('/third', function (req, res) {
   res.send("REQUEST TTHIRD  RECEIVED");
 });
 
-app.get('/htfirst', function (req, res) {
-  res.sendFile(path.join(_dirname,  'ui','first1.html'));
-});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
