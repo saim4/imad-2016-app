0ff30eada271444cef0fd8e7f6eee174f772df8f
saim4 <saimyusuf4@gmail.com>
2016-10-15 12:21:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,17 @@ console.log('Loaded!');
 //converting text of index.html
 var element= document.getElementById('main-text');
 element.innerHTML= 'SAIM"S TESTBENCH.THIS IS FROM ONLINE SERVER';
+
 //moving the madi image from index.html
 var img= document.getElementById('madi');
+var marginLeft= 0;
+function moveRight()
+{
+    
+    marginLeft= +10;
+    img.style.marginLeft= marginLeft+ 'px';
+}
 img.onclick= function(){
     
-    img.style.marginLeft= '100px';
+    var time = setInterval(moveRight,10);
 };
