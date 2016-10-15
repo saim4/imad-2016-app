59ac6e7d86408ae845582c8cb53b250e9aa44800
saim4 <saimyusuf4@gmail.com>
2016-10-15 12:09:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,11 @@
 console.log('Loaded!');
+
+//converting text of index.html
 var element= document.getElementById('main-text');
 element.innerHTML= 'SAIM"S TESTBENCH.THIS IS FROM ONLINE SERVER';
+//moving the madi image from index.html
+var img= document.getElementById('madi');
+img.onclick= function(){
+    
+    img.style.marginLeft= '100px';
+};
