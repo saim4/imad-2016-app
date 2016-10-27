f95578a177cf885e488fbd967c1a2226778b0684
saim4 <saimyusuf4@gmail.com>
2016-10-27 10:22:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles= {
- 'article-one' : {
+ 'articleone' : {
   title: 'ARTICLE ONE| SAIM YUSUF',
   heading: 'ARTICLE ONE',
   date: '4.12.1995',
