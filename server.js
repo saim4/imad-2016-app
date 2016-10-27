ca0368cf897310ef25f583b8f8342e54d7ecdc81
saim4 <saimyusuf4@gmail.com>
2016-10-27 13:24:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -140,6 +140,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter= 0;
+
+app.get('/counter',function(req,res){
+    
+counter= counter+1;
+res.send(counter.toString());
+});
 
 app.get('/:articlename', function (req, res) {
      //articlename== article-one
