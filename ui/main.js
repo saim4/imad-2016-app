077046ba7c784bfed6ce08ef0fd69ab776f0d4fc
saim4 <saimyusuf4@gmail.com>
2016-10-28 18:51:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,34 @@ img.onclick= function(){
     
     var interval = setInterval(moveRight,10);
 };
+
+
+var button= document.getElementById('counter');
+
+button.onclick = function(){
+    
+//Creating request to counter page
+
+var request= new XMLHttpRequest();
+
+//handling request
+
+request.onreadystatechange= function(){
+    
+    if(request.readyState === XMLHttpRequest.DONE ){
+    if(request.status===200){
+        var counter= request.responseText;
+        var span= document.getElementById('count');
+        span.innerHTML = counter.toString();
+    }    
+        
+    }
+};
+
+request.open('GET','http://saim4.imad.hasura-app.io/counter',true)
+request.send(null);
+} ;
+
+
+
+
