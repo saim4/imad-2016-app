b3dda2dc17b3fce3405c32da4d31a474975bc43d
saim4 <saimyusuf4@gmail.com>
2016-11-24 12:03:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -182,6 +182,10 @@ app.get('/ui/Rotatory solar panel-Copy.pptm',function(req,res){
     res.sendFile(path.join(_dirname, 'ui', 'Rotatory solar panel-Copy.pptm'))
 });
 
+app.get('/ui/chk_captcha.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'chk_captcha.jpg'));
+});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
