a4f28ae25748289408b2e0fbc4102b05761079d4
saim4 <saimyusuf4@gmail.com>
2016-11-30 18:01:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,7 +35,7 @@ request.onreadystatechange= function(){
     if(request.status===200){
         var counter= request.responseText;
         var span= document.getElementById('count');
-        span.innerHTML = counter.toString();
+        span.innerHTML = count.toString();
     }    
         
     }
